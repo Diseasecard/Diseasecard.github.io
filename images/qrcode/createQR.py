66d292a3f726f2c9bead0a4a0e5c9f2d911612a0
GuiAmorim03 <guilhermeamorim@ua.pt>
2024-06-05 00:40:47 +0100

    qr code update

--- images/qrcode/createQR.py
+++ images/qrcode/createQR.py
@@ -1,7 +1,7 @@
 import qrcode
 from PIL import Image, ImageDraw
 
-data = "https://drive.google.com/file/d/1CeIrN3rIf8Np9Uz4U3X70kfkGtHy71CN/view?usp=sharing"
+data = "https://github.com/Diseasecard/Diseasecard.github.io/raw/main/apk/diseasecard.apk"
 
 qr = qrcode.QRCode(
     version=1,
@@ -18,18 +18,23 @@ img_qr = qr.make_image(fill_color='black', back_color='white').convert('RGBA')
 logo = Image.open('images/qrcode/dna.png')
 
 logo_size = 100
+border_size = 20 
 
-logo_bg = Image.new('RGBA', (logo_size, logo_size), 'white')
+logo_with_border_size = logo_size + 2 * border_size
+logo_with_border = Image.new('RGBA', (logo_with_border_size, logo_with_border_size), 'white')
+
 logo = logo.resize((logo_size, logo_size), Image.LANCZOS)
-logo_bg.paste(logo, (0, 0), logo)
 
-mask = Image.new('L', (logo_size, logo_size), 0)
+logo_pos = (border_size, border_size)
+logo_with_border.paste(logo, logo_pos, logo)
+
+mask = Image.new('L', (logo_with_border_size, logo_with_border_size), 0)
 draw = ImageDraw.Draw(mask)
-draw.ellipse((0, 0, logo_size, logo_size), fill=255)
+draw.ellipse((0, 0, logo_with_border_size, logo_with_border_size), fill=255)
 
-logo_circular = Image.composite(logo_bg, Image.new('RGBA', (logo_size, logo_size)), mask)
+logo_circular = Image.composite(logo_with_border, Image.new('RGBA', (logo_with_border_size, logo_with_border_size)), mask)
 
-pos = ((img_qr.size[0] - logo_size) // 2, (img_qr.size[1] - logo_size) // 2)
+pos = ((img_qr.size[0] - logo_with_border_size) // 2, (img_qr.size[1] - logo_with_border_size) // 2)
 
 img_qr.paste(logo_circular, pos, mask)
 
